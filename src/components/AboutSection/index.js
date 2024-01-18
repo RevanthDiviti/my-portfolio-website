e4e93138addd90e2119c842d0eb6a9277af62ab3
RevanthDiviti <145599691+RevanthDiviti@users.noreply.github.com>
2024-01-18 10:12:56 +0000

    Updates

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -3,7 +3,7 @@ import themeContext from "../../themeContext/themeContext";
 
 import { AiFillHtml5, AiFillGithub } from "react-icons/ai";
 import { DiCss3, DiReact } from "react-icons/di";
-import { FaBootstrap, FaNodeJs, FaGitAlt } from "react-icons/fa";
+import {  FaGitAlt } from "react-icons/fa";
 import { RiJavascriptFill } from "react-icons/ri";
 import { SiExpress, SiNpm } from "react-icons/si";
 
@@ -41,14 +41,14 @@ function AboutSection() {
           <Description>
             I'm a full-stack developer with a passion for using technology to
             solve real-world problems. I learned full stack development from
-            Nxtwave, a leading software training institute in India.
+            Vcube Software Solutions(Hyderabad).
           </Description>
           <Description>
-            During my training at Nxtwave, I gained a strong understanding of
+            During my training at Vcube Software Solutions(Hyderabad), I gained a strong understanding of
             both front-end and back-end technologies, including HTML, CSS,
-            Bootstrap, JavaScript, React.js, Node.js, Express.js, and SQLite. I
+            JavaScript, React.js, Python, Django, and MySQL. {/*I
             also have experience with a variety of other technologies, such as
-            Git, and GitHub.
+            Git, and GitHub. */}
           </Description>
           <Description>
             I'm a highly motivated and results-oriented individual. I'm always
@@ -58,7 +58,7 @@ function AboutSection() {
             common goals.
           </Description>
           <Description>
-            I'm excited to Being as a full-stack developer, and I'm confident
+            I'm excited to being as a full-stack developer, and I'm confident
             that I can make a significant contribution to your's success. I'm
             also open to learning new technologies and taking on new challenges.
           </Description>
@@ -68,54 +68,7 @@ function AboutSection() {
         <TechStackHeading>My core tools and technologies</TechStackHeading>
         <Tools>
           <ToolList>
-            <ToolItem>
-              <AiFillHtml5
-                color="#ff4d15"
-                style={{
-                  marginRight: "0.5rem",
-                }}
-              />
-              HTML
-            </ToolItem>
-            <ToolItem>
-              <DiCss3
-                color="#264de4"
-                style={{
-                  marginRight: "0.5rem",
-                }}
-              />
-              CSS
-            </ToolItem>
-            <ToolItem>
-              <FaBootstrap
-                color="#7a09f7"
-                style={{
-                  marginRight: "0.5rem",
-                }}
-              />
-              Bootstrap
-            </ToolItem>
-          </ToolList>
-          <ToolList>
-            <ToolItem>
-              <RiJavascriptFill
-                color="#f0db4f"
-                style={{
-                  marginRight: "0.5rem",
-                }}
-              />
-              Javascript
-            </ToolItem>
-            <ToolItem>
-              <DiReact
-                color="#61dbfb"
-                style={{
-                  marginRight: "0.5rem",
-                }}
-              />
-              React.JS
-            </ToolItem>
-            <ToolItem>
+          <ToolItem>
               <img
                 src="https://res.cloudinary.com/dmlhm8dwi/image/upload/v1688022486/Python-logo-notext.svg_uvzbbk.png"
                 style={{
@@ -126,6 +79,64 @@ function AboutSection() {
               />
               Python
             </ToolItem>
+            <ToolItem>
+              <AiFillHtml5
+                color="#ff4d15"
+                style={{
+                  marginRight: "0.5rem",
+                }}
+              />
+              HTML
+            </ToolItem>
+            <ToolItem>
+              <RiJavascriptFill
+                color="#f0db4f"
+                style={{
+                  marginRight: "0.5rem",
+                }}
+              />
+              Javascript
+            </ToolItem>
+            {/* <ToolItem>
+              <FaBootstrap
+                color="#7a09f7"
+                style={{
+                  marginRight: "0.5rem",
+                }}
+              />
+              Bootstrap
+            </ToolItem> */}
+          </ToolList>
+          <ToolList>
+          <ToolItem>
+              <img
+                src="https://res.cloudinary.com/dmlhm8dwi/image/upload/v1688022486/Python-logo-notext.svg_uvzbbk.png"
+                style={{
+                  width: "1rem",
+                  marginRight: "0.5rem",
+                }}
+                alt="Python Icon"
+              />
+              Django
+            </ToolItem>
+            <ToolItem>
+              <DiCss3
+                color="#264de4"
+                style={{
+                  marginRight: "0.5rem",
+                }}
+              />
+              CSS
+            </ToolItem> 
+            <ToolItem>
+              <FaGitAlt
+                color="#f05030"
+                style={{
+                  marginRight: "0.5rem",
+                }}
+              />
+              Git
+            </ToolItem>           
           </ToolList>
           <ToolList>
             <ToolItem>
@@ -137,9 +148,18 @@ function AboutSection() {
                 }}
                 alt="SQLite Icon"
               />
-              SQLite
+              MySQL
             </ToolItem>
             <ToolItem>
+              <DiReact
+                color="#61dbfb"
+                style={{
+                  marginRight: "0.5rem",
+                }}
+              />
+              React.JS
+            </ToolItem> 
+            {/* <ToolItem>
               <FaNodeJs
                 color="#68a063"
                 style={{
@@ -147,35 +167,7 @@ function AboutSection() {
                 }}
               />
               Node.JS
-            </ToolItem>
-            <ToolItem>
-              <SiExpress
-                style={{
-                  marginRight: "0.5rem",
-                }}
-              />{" "}
-              Express.JS
-            </ToolItem>
-          </ToolList>
-          <ToolList>
-            <ToolItem>
-              <SiNpm
-                color="#cb3837"
-                style={{
-                  marginRight: "0.5rem",
-                }}
-              />
-              NPM
-            </ToolItem>
-            <ToolItem>
-              <FaGitAlt
-                color="#f05030"
-                style={{
-                  marginRight: "0.5rem",
-                }}
-              />
-              Git
-            </ToolItem>
+            </ToolItem> */}
             <ToolItem>
               <AiFillGithub
                 style={{
@@ -183,40 +175,6 @@ function AboutSection() {
                 }}
               />
               GitHub
-            </ToolItem>
-          </ToolList>
-          <ToolList>
-            <ToolItem>
-              <img
-                src="https://res.cloudinary.com/dmlhm8dwi/image/upload/v1688022067/pngwing.com_rte7ts.png"
-                style={{
-                  width: "1rem",
-                  marginRight: "0.5rem",
-                }}
-                alt="Figma Icon"
-              />
-              Figma
-            </ToolItem>
-            <ToolItem>
-              <img
-                src="https://res.cloudinary.com/dmlhm8dwi/image/upload/v1688023219/mongodb_original_logo_icon_146424_gfauqe.png"
-                style={{
-                  width: "1rem",
-                  marginRight: "0.5rem",
-                }}
-                alt="MongoDB Icon"
-              />
-              MongoDB
-              <sup
-                style={{
-                  color: "#47b147",
-                  fontSize: "0.54rem",
-                  fontFamily: "Roboto, Mono monospace",
-                  marginLeft: "0.2rem",
-                }}
-              >
-                SOON!
-              </sup>
             </ToolItem>
           </ToolList>
         </Tools>
